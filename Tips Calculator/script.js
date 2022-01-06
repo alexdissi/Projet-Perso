2d041ebb80728e29e97690e90118036d8f6aebfc
Alexandre <alexandredissi@icloud.com>
2022-01-06 01:43:05 +0100

    ajout du JS

---
+++ Tips Calculator/script.js
@@ -0,0 +1,8 @@
+const inputs = document.querySelectorAll('.calculatorInput');
+const buttons = document.querySelectorAll('.selectTip');
+const tipValue = document.getElementById('tip');
+const billValue = document.getElementById('bill');
+const peopleValue = document.getElementById('nbrPeople');
+const tipAmount = document.getElementById('tip-amount');
+const totalPer = document.getElementById('total-per');
+
